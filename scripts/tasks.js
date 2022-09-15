98d7d380575d514bf77f794adcd4a80cf9867644
LuzSallietti <luzsallietti@gmail.com>
2022-09-15 15:57:34 -0300

    obtener listado de tareas / crear nueva tarea / renderizar tareas 1era parte

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -12,7 +12,13 @@ window.addEventListener('load', function () {
   /* ---------------- variables globales y llamado a funciones ---------------- */
   const btnCerrarSesion = document.querySelector('#closeApp');
   const username = document.querySelector('.user-info p');
+  const formCrearTarea = document.querySelector('.nueva-tarea');
+  const nuevaTarea = document.querySelector('#nuevaTarea');
+  const tareasPendientes = document.querySelector('.tareas-pendientes');
+  const tareasTerminadas = document.querySelector('.tareas-terminadas');
+
   obtenerNombreUsuario();
+  consultarTareas();
 
 
 
@@ -55,11 +61,25 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
 
   function consultarTareas() {
+    let jwt = cargarToken();    
+    const URL = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
+    const config = {
+      method: 'GET',
+      headers: {
+          'Content-Type' : "application/json; charset=UTF-8",
+          'authorization' : jwt
+      }
+  }
+  fetch(URL, config).then( resp => {
+    return resp.json()
+  }
+  ).then (tareas => {
+    console.log(tareas);
+    if(tareas.length > 0){
+      renderizarTareas(tareas);
+    }    
+  })    
     
-    
-
-
-
   };
 
 
@@ -68,10 +88,31 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
 
   formCrearTarea.addEventListener('submit', function (event) {
-    
-
-
-
+    event.preventDefault();
+    let jwt = cargarToken();
+    const URL = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
+    const tarea = {
+      description: nuevaTarea.value,
+      completed : false
+    }
+    //se podría validar que el input no este vacío
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type' : "application/json; charset=UTF-8",
+        'authorization' : jwt
+      },
+      body: JSON.stringify(tarea)
+  }   
+    fetch(URL, config).then( resp => {
+      return resp.json()
+    }
+    ).then (data => {
+      //limpio el input del form
+      nuevaTarea.value='';
+      console.log(data);
+      // llamar a la funcion renderizarTareas    
+    })           
 
   });
 
@@ -80,13 +121,22 @@ window.addEventListener('load', function () {
   /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
   /* -------------------------------------------------------------------------- */
   function renderizarTareas(listado) {
-
-
-
-
-
-
-
+    listado.forEach(tarea => {
+      console.log(tarea)
+      if(tarea.completed){
+        let li = document.createElement('li');
+        li.setAttribute('class', 'tarea');        
+        li.textContent = tarea.description;        
+        //faltan elementos y clases --> icono y btn
+        tareasTerminadas.appendChild(li);
+      } else {
+        let li = document.createElement('li');
+        li.setAttribute('class', 'tarea');
+        li.textContent = tarea.description;
+        tareasPendientes.appendChild(li);
+        
+      }      
+    });    
   };
 
   /* -------------------------------------------------------------------------- */
